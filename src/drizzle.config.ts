e950d7f1f5685d7adb81e80eaecc3c6f705b3b10
alicezuberg930 <tien23851@gmail.com>
2025-10-31 21:41:47 +0700

    remove secret

--- src/drizzle.config.ts
+++ src/drizzle.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
     schema: './db/schemas',
     dialect: 'mysql',
     dbCredentials: {
-        url: "mysql://avnadmin:AVNS_KfaROSf_5YxC3WhSOuW@zingmp3-mysql-music-website-db.h.aivencloud.com:17444/defaultdb",
+        url: "",
     },
 });

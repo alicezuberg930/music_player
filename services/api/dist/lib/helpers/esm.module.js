9c5a4cf36da566c015f79fd86f3bddf88dbac26d
alicezuberg930 <tien23851@gmail.com>
2025-12-22 00:42:48 +0700

    chore: remove cached build file

--- services/api/dist/lib/helpers/esm.module.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.esmMusicMetadata = exports.esmFileType = void 0;
-const esmFileType = async () => await import('file-type');
-exports.esmFileType = esmFileType;
-const esmMusicMetadata = async () => await import('music-metadata');
-exports.esmMusicMetadata = esmMusicMetadata;
--- services/api/dist/middleware/notfound.middleware.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.notFoundHandlerMiddleware = notFoundHandlerMiddleware;
-const exception_1 = require("@yukikaze/lib/exception");
-function notFoundHandlerMiddleware(req, _res, next) {
-    next(new exception_1.NotFoundException(`Route or Method not found. Cannot ${req.method} ${req.originalUrl}`));
-}
